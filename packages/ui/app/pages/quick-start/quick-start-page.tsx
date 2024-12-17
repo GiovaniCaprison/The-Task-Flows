34268e3c0e41c770d6857efc73d10bc73b055de3
Louis Grennell <icantfindanadressname@gmail.com>
2024-12-17 17:56:08 +0000

    Cognito needed proper scopes for GetUser API Lambda needed correct permissions for both Cognito and S3 API Gateway needed permission to invoke Lambda The client URL structure needed to be different between dev and prod CORS headers were required in the Lambda response

--- packages/ui/app/pages/quick-start/quick-start-page.tsx
+++ packages/ui/app/pages/quick-start/quick-start-page.tsx
@@ -3,22 +3,26 @@ import { PageHeader } from '../../components/page-header';
 import { DropZoneComponent } from "../../components/drop-zone-component";
 import { FileManager } from "./file-manager-component";
 import {PageSection} from "../../components/page-section";
-import Container from "@mui/material/Container";
-import {Text} from "../../components/text";
+// import Container from "@mui/material/Container";
+// import {Text} from "../../components/text";
+
+/*
+<PageSection>
+  <Container
+    sx={{paddingTop: '40px', paddingBottom: '40px', textAlign: 'center'}}
+    >
+    <Text variant={'h6'}>
+        TheTaskFlows is currently in Development, meaning it's features are unavailable to any non-admin users. Updates will be coming shortly.
+    </Text>
+  </Container>
+</PageSection>
+ */
 
 export const QuickStartPage: FunctionComponent = () => {
     return (
     <>
       <PageHeader />
-      <PageSection>
-          <Container
-                sx={{paddingTop: '40px', paddingBottom: '40px', textAlign: 'center'}}
-            >
-                <Text variant={'h6'}>
-                    TheTaskFlows is currently in Development, meaning it's features are unavailable to any non-admin users. Updates will be coming shortly.
-                </Text>
-          </Container>
-      </PageSection>
+
       <PageSection>
           <FileManager />
           <DropZoneComponent />
